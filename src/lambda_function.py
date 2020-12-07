3dd25e983615afda91a9fc90138c805172989781
Naoya Sawaguchi <naoyasawaguchi978@gmail.com>
2020-12-07 11:37:39 +0900

    リクエストが失敗する不具合を修正

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -27,28 +27,29 @@ def main():
     channel = os.environ['SLACK_CHANNEL']
     username = os.environ['SLACK_USERNAME']
     icon_emoji = os.environ['SLACK_ICON_EMOJI']
-    text = 'Hello, world from Lambda! make'
 
     ec2 = stop_instances.get_ec2_client()
     rds = stop_instances.get_rds_client()
     stopped_ec2_instances = stop_instances.stop_running_ec2_instances(ec2)
     stopped_rds_instances = stop_instances.stop_running_db_instances(rds)
 
-    text = ""
+    text = ''
     if len(stopped_ec2_instances) > 0:
-        text += "*" + str(len(stopped_ec2_instances)) + "個のEC2インスタンスを停止させました*\n"
+        text += '*' + str(len(stopped_ec2_instances)) + \
+            '個のEC2インスタンスを停止させました*\n'
         for i in stopped_ec2_instances:
-            text += " - *" + i + "*\n"
-        text += "\n"
-    
+            text += ' - *' + i + '*\n'
+        text += '\n'
+
     if len(stopped_rds_instances) > 0:
-        text += "*" + str(len(stopped_rds_instances)) + "個のRDSインスタンスを停止させました*\n"
+        text += '*' + str(len(stopped_rds_instances)) + \
+            '個のRDSインスタンスを停止させました*\n'
         for i in stopped_rds_instances:
-            text += " - *" + i + "*\n"
-        text += "\n"
-    
-    if len(stopped_ec2_instances) == 0 and len(stopped_rds_instances):
-        text += "停止するインスタンスはありませんでした。:tada:"
+            text += ' - *' + i + '*\n'
+        text += '\n'
+
+    if len(stopped_ec2_instances) == 0 and len(stopped_rds_instances) == 0:
+        text += '停止するインスタンスはありませんでした。:tada:'
 
     return post_to_slack(
         webhook_url=webhook_url,
@@ -57,6 +58,7 @@ def main():
         icon_emoji=icon_emoji,
         text=text
     )
+
 
 def lambda_handler(event, context):
     return main()
